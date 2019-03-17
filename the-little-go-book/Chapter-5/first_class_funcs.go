d7a26f887464a7335f28024df925927fbfd994c3
Adriel Martinez <adriel@adrielm.ca>
2019-03-17 02:03:18 -0400

    finish chapter 5

--- the-little-go-book/Chapter-5/first_class_funcs.go
+++ the-little-go-book/Chapter-5/first_class_funcs.go
@@ -15,6 +15,24 @@ func subber(a int, b int) int {
 	return a - b
 }
 
-func applyer(a []int, f Applyer) {
-	ret := make([]int, len(a))
+func applyer(a []int, f Applyer) ([]int) {
+	aLen := len(a)
+	ret := make([]int, aLen)
+	apply10 := func(some int) (int) {
+		return f(some, 10)
+	}
+	for i := 0; i < aLen; i++ {
+		ret[i] = apply10(a[i])
+	}
+	return ret
+}
+
+func main() {
+	ok1 := []int{1, 2, 3}
+	ok2 := make([]int, 5)
+	for i := range(ok2) {
+		ok2[i] = i + 2
+	}
+	fmt.Println(applyer(ok1, adder))
+	fmt.Println(applyer(ok2, subber))
 }
